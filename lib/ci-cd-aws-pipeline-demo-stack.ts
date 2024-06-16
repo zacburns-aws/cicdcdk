1541d49f929609ffa96512434531fecb1cabe697
Zac Burns <burnz@amazon.com>
2024-06-16 18:14:23 +0100

    6th commit

--- lib/ci-cd-aws-pipeline-demo-stack.ts
+++ lib/ci-cd-aws-pipeline-demo-stack.ts
@@ -8,7 +8,7 @@ export class CiCdAwsPipelineDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'Pipeline', {
+   const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'TestPipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('zacburns-aws/cicdcdk', 'main'), //Remember to change 
